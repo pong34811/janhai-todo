8a9b23928b60567b76ae877fe8fd8d4be710a0ba
Waritnan <pong34811@gmail.com>
2024-10-18 17:44:57 +0700

    asdasd

--- sabai_backend/apps/users/views.py
+++ sabai_backend/apps/users/views.py
@@ -2,8 +2,23 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]  # ให้แน่ใจว่าผู้ใช้ล็อกอิน
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        # ส่งกลับ token และข้อมูลผู้ใช้ในบรรทัดเดียว
+        return Response({
+            'user': UserSerializer(serializer.user).data,
+            'refresh': serializer.validated_data['refresh'],
+            'access': serializer.validated_data['access'],
+        })
--- sabai_backend/sabai_backend/urls.py
+++ sabai_backend/sabai_backend/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from apps.users.views import UserViewSet
+from apps.users.views import CustomTokenObtainPairView, UserViewSet
 from apps.boards.views import BoardViewSet ,ListViewSet
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -19,6 +18,6 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),  # Admin site
 
     # Token URLs
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
