64381499afb4b156dd9449a42b422a9c8641d875
Waritnan <pong34811@gmail.com>
2024-10-30 17:25:35 +0700

    add route path register url

--- sabai_frontend/src/routes/CustomRoutes.js
+++ sabai_frontend/src/routes/CustomRoutes.js
@@ -6,6 +6,7 @@ import NoPage404 from "../screen/NoPage404";
 import BoardView from "../screen/BoardView";
 import HeaderView from '../screen/HeaderView';
 import ProtectedRoute from './ProtectedRoute';
+import RegisterView from '../screen/RegisterView';
 
 
 
@@ -15,6 +16,7 @@ function CustomRoute() {
       <Routes>
 
         <Route path="/login" element={<LoginView />} />
+        <Route path="/register" element={<RegisterView />} />
 
 
         <Route path="/" element={<HeaderView />}>
