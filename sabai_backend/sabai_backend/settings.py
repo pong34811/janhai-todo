0e63b0aa90f53a741b0d5aa1f00ff3b69952cd46
Waritnan <pong34811@gmail.com>
2024-11-01 17:42:52 +0700

    fix access token jwt 15day retoken 7day

--- sabai_backend/sabai_backend/settings.py
+++ sabai_backend/sabai_backend/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),  
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,  
