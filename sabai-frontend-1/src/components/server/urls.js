a6c0e25d99ae86abdbdb0d15befe01b5b8911286
Waritnan <pong34811@gmail.com>
2024-11-21 16:22:32 +0700

    delete 24 files

--- sabai-frontend-1/src/components/server/urls.js
+++
@@ -1,6 +0,0 @@
-export default {
-  LoginAPI: 'http://localhost:8000/api/login/',
-  RegisterAPI: 'http://localhost:8000/api/register/',
-  BoardAPI: 'http://localhost:8000/api/boards/',
-  // Add more URLs as needed
-};
