8bc660285c51e68eaa9f530a99d4a764262189e4
Waritnan <pong34811@gmail.com>
2024-11-26 11:40:38 +0700

    chore: delete start_npm.bat and start_python.bat

--- start_npm.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "D:\GIT\janhai-todo\sabai_frontend"
-npm start
-pause
--- start_python.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "D:\GIT\janhai-todo"
-call venv\Scripts\activate.bat
-
-cd /d "D:\GIT\janhai-todo\sabai_backend"
-python manage.py runserver
-pause
