20aeb6a76473fb71ee98143b97d71c48abc193b5
Waritnan <pong34811@gmail.com>
2024-12-13 16:48:17 +0700

    update url ChangePasswordAPI

--- sabai_frontend/src/routes/CustomAPI.js
+++ sabai_frontend/src/routes/CustomAPI.js
@@ -1,11 +1,12 @@
 export const URL_AUTH = {
-    LoginAPI: "http://localhost:8000/api/login/",
-    RegisterAPI: "http://localhost:8000/api/users/",
-    BoardAPI: "http://localhost:8000/api/boards/",
-    ListsAPI: "http://localhost:8000/api/lists/",
-    ReorderListsAPI: "http://localhost:8000/api/lists/reorder/",
-    TasksAPI: "http://localhost:8000/api/tasks/",
-    PasswordResetAPI: "http://localhost:8000/api/password_reset/",
-    PasswordResetConfirmAPI: "http://localhost:8000/api/password_reset/confirm/", 
-    UsersAPI: "http://localhost:8000/api/users/", // เพิ่ม URL สำหรับ Users
+  LoginAPI: "http://localhost:8000/api/login/",
+  RegisterAPI: "http://localhost:8000/api/users/",
+  BoardAPI: "http://localhost:8000/api/boards/",
+  ListsAPI: "http://localhost:8000/api/lists/",
+  ReorderListsAPI: "http://localhost:8000/api/lists/reorder/",
+  TasksAPI: "http://localhost:8000/api/tasks/",
+  PasswordResetAPI: "http://localhost:8000/api/password_reset/",
+  PasswordResetConfirmAPI: "http://localhost:8000/api/password_reset/confirm/",
+  UsersAPI: "http://localhost:8000/api/users/", // เพิ่ม URL สำหรับ Users
+  ChangePasswordAPI: "http://localhost:8000/api/change_password/"
 };
