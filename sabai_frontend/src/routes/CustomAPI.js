f4c3bde6156294e2df7221657e942ce18fe0d168
EvarinDev <evarin.dev@gmail.com>
2024-12-18 20:31:21 +0700

    fix: update API URLs to use relative paths

--- sabai_frontend/src/routes/CustomAPI.js
+++ sabai_frontend/src/routes/CustomAPI.js
@@ -1,12 +1,12 @@
 export const URL_AUTH = {
-  LoginAPI: "http://localhost:8000/api/login/",
-  RegisterAPI: "http://localhost:8000/api/users/",
-  BoardAPI: "http://localhost:8000/api/boards/",
-  ListsAPI: "http://localhost:8000/api/lists/",
-  ReorderListsAPI: "http://localhost:8000/api/lists/reorder/",
-  TasksAPI: "http://localhost:8000/api/tasks/",
-  PasswordResetAPI: "http://localhost:8000/api/password_reset/",
-  PasswordResetConfirmAPI: "http://localhost:8000/api/password_reset/confirm/",
-  UsersAPI: "http://localhost:8000/api/users/", // เพิ่ม URL สำหรับ Users
-  ChangePasswordAPI: "http://localhost:8000/api/change_password/"
+  LoginAPI: "/api/login/",
+  RegisterAPI: "/api/users/",
+  BoardAPI: "/api/boards/",
+  ListsAPI: "/api/lists/",
+  ReorderListsAPI: "/api/lists/reorder/",
+  TasksAPI: "/api/tasks/",
+  PasswordResetAPI: "/api/password_reset/",
+  PasswordResetConfirmAPI: "/api/password_reset/confirm/",
+  UsersAPI: "/api/users/", // เพิ่ม URL สำหรับ Users
+  ChangePasswordAPI: "/api/change_password/"
 };
