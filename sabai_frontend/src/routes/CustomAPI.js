82abde7fb1c1c1c5c978acf726d16035ba2004a6
pong34811 <pong34811@gmail.com>
2024-12-20 20:00:02 +0700

    fix url customapi

--- sabai_frontend/src/routes/CustomAPI.js
+++ sabai_frontend/src/routes/CustomAPI.js
@@ -3,10 +3,10 @@ export const URL_AUTH = {
   RegisterAPI: "https://api.janhai.space/api/users/",
   BoardAPI: "https://api.janhai.space/api/boards/",
   ListsAPI: "https://api.janhai.space/api/lists/",
-  ReorderListsAPI: "/api/lists/reorder/",
+  ReorderListsAPI: "https://api.janhai.space/api/lists/reorder/",
   TasksAPI: "https://api.janhai.space/api/tasks/",
-  PasswordResetAPI: "/api/password_reset/",
-  PasswordResetConfirmAPI: "/api/password_reset/confirm/",
+  PasswordResetAPI: "https://api.janhai.space/api/password_reset/",
+  PasswordResetConfirmAPI: "https://api.janhai.space/api/password_reset/confirm/",
   UsersAPI: "https://api.janhai.space/api/users/", // เพิ่ม URL สำหรับ Users
   ChangePasswordAPI: "https://api.janhai.space/api/change_password/"
 };
